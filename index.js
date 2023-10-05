224196b1f5ea644945de06385a18e46499f40b45
PavelMart <martackovp@yandex.ru>
2023-10-05 23:56:35 +0300

    Бот настроен на работу с gpt-3.5-turbo

--- index.js
+++ index.js
@@ -10,174 +10,196 @@ let apiKey = null;
 
 let isBusy = false;
 
+let document = [];
+
 const getResult = async (chatId, json) => {
-  const configuration = new Configuration({
-    apiKey,
-  });
+    const configuration = new Configuration({
+        apiKey,
+    });
 
-  const openai = new OpenAIApi(configuration);
+    const openai = new OpenAIApi(configuration);
 
-  const sendCompletion = async (paragraph) => {
-    try {
-      if (!paragraph.handle || !paragraph.text) {
-        return "";
-      }
+    const sendCompletion = async (paragraph) => {
+        try {
+            if (!paragraph.handle || !paragraph.text) {
+                return "";
+            }
 
-      const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${paragraph.query}: "${paragraph.text}"`,
-        temperature: 0.7,
-        max_tokens: 2048,
-      });
+            // const completion = await openai.createCompletion({
+            //     model: "gpt-3.5-turbo",
+            //     prompt: `${paragraph.query}: "${paragraph.text}"`,
+            //     temperature: 0.2,
+            //     max_tokens: 2048,
+            // });
 
-      return completion.data.choices[0].text.trim();
-    } catch (error) {
-      throw error;
+            // return completion.data.choices[0].text.trim();
+
+            const stream1 = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: [
+                    { role: "system", content: `${paragraph.query}` },
+                    { role: "user", content: `${paragraph.text}` },
+                ],
+                stream: false,
+            });
+
+            // const stream = await openai.createChatCompletion({
+            //     model: "gpt-3.5-turbo",
+            //     messages: [
+            //         { role: "system", content: `${paragraph.query_2}` },
+            //         { role: "user", content: `${stream1.data.choices[0].message.content}` },
+            //     ],
+            //     stream: false,
+            // });
+
+            return stream1.data.choices[0].message.content;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    const handleParagraphs = async (paragraphs) => {
+        const promisesList = paragraphs.map(sendCompletion);
+
+        const answersList = await Promise.all(promisesList);
+
+        const resultsList = paragraphs.map((p, i) => ({ ...p, text: answersList[i] }));
+
+        return resultsList;
+    };
+
+    const paragraphs = json;
+
+    const output = [];
+
+    const count = 60;
+
+    const pages = Math.floor(paragraphs.length / count);
+
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+    await bot.sendMessage(chatId, `Обработано 0/${paragraphs.length} параграфов`);
+
+    for (let page = 0; page <= pages; page++) {
+        let start = Date.now();
+        const result = await handleParagraphs(paragraphs.slice(page * count, (page + 1) * count));
+        output.push(...result);
+        if (page < pages) {
+            await bot.sendMessage(chatId, `Обработано ${(page + 1) * count}/${paragraphs.length} параграфов`);
+            await delay(60000 - (Date.now() - start));
+        } else await bot.sendMessage(chatId, `Обработано ${paragraphs.length}/${paragraphs.length} параграфов`);
     }
-  };
 
-  const handleParagraphs = async (paragraphs) => {
-    const promisesList = paragraphs.map(sendCompletion);
-
-    const answersList = await Promise.all(promisesList);
-
-    const resultsList = paragraphs.map((p, i) => ({ ...p, text: answersList[i] }));
-
-    return resultsList;
-  };
-
-  const paragraphs = json;
-
-  const output = [];
-
-  const count = 60;
-
-  const pages = Math.floor(paragraphs.length / count);
-
-  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-
-  await bot.sendMessage(chatId, `Обработано 0/${paragraphs.length} параграфов`);
-
-  for (let page = 0; page <= pages; page++) {
-    let start = Date.now();
-    const result = await handleParagraphs(paragraphs.slice(page * count, (page + 1) * count));
-    output.push(...result);
-    if (page < pages) {
-      await bot.sendMessage(chatId, `Обработано ${(page + 1) * count}/${paragraphs.length} параграфов`);
-      await delay(60000 - (Date.now() - start));
-    } else await bot.sendMessage(chatId, `Обработано ${paragraphs.length}/${paragraphs.length} параграфов`);
-  }
-
-  return output;
+    return output;
 };
 
 const getReadStreamPromise = (stream, fileWriter) => {
-  return new Promise((resolve, reject) => {
-    stream.on("data", (chunk) => {
-      fileWriter.write(chunk);
+    return new Promise((resolve, reject) => {
+        stream.on("data", (chunk) => {
+            fileWriter.write(chunk);
+        });
+        stream.on("error", (err) => {
+            reject(err);
+        });
+        stream.on("end", () => {
+            resolve();
+        });
     });
-    stream.on("error", (err) => {
-      reject(err);
-    });
-    stream.on("end", () => {
-      resolve();
-    });
-  });
 };
 
 const bot = new TelegramBot(TELEGRAM_BOT_API_KEY, { polling: true });
 
 bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
+    const chatId = msg.chat.id;
 
-  try {
-    if (isBusy) throw new Error("В данный момент обрабатывается другой документ, повторите позднее");
+    try {
+        if (isBusy) throw new Error("В данный момент обрабатывается другой документ, повторите позднее");
 
-    if (msg.text && msg.text.includes("API_KEY=")) {
-      apiKey = msg.text.split("=")[1];
-      fs.writeFile("api.txt", apiKey, (err) => {
-        if (err) throw err;
-      });
-    } else apiKey = fs.readFileSync("api.txt").toString();
+        if (msg.text && msg.text.includes("API_KEY=")) {
+            apiKey = msg.text.split("=")[1];
+            fs.writeFile("api.txt", apiKey, (err) => {
+                if (err) throw err;
+            });
+        } else apiKey = fs.readFileSync("api.txt").toString();
 
-    if (!apiKey) return await bot.sendMessage(chatId, "Пришлите API_KEY в формате: API_KEY=ваш_ключ_api");
+        if (!apiKey) return await bot.sendMessage(chatId, "Пришлите API_KEY в формате: API_KEY=ваш_ключ_api");
 
-    if (msg.text === "/get_api_key") return await bot.sendMessage(chatId, apiKey);
+        if (msg.text === "/get_api_key") return await bot.sendMessage(chatId, apiKey);
 
-    if (!msg.document) return await bot.sendMessage(chatId, "Пожалуйста, отправьте документ Word");
+        if (!msg.document) return await bot.sendMessage(chatId, "Пожалуйста, отправьте документ Word");
 
-    const ext = msg.document.file_name.split(".").at(-1);
+        const ext = msg.document.file_name.split(".").at(-1);
 
-    if (ext !== "docx") return await bot.sendMessage(chatId, "Некорректный файл. Расширение обрабатываемого файла должно быть .docx");
+        if (ext !== "docx") return await bot.sendMessage(chatId, "Некорректный файл. Расширение обрабатываемого файла должно быть .docx");
 
-    isBusy = true;
+        isBusy = true;
 
-    await bot.sendMessage(chatId, "Подождите, идет обработка файла");
+        await bot.sendMessage(chatId, "Подождите, идет обработка файла");
 
-    const fileWriter = fs.createWriteStream("file.docx");
+        const fileWriter = fs.createWriteStream("file.docx");
 
-    const stream = bot.getFileStream(msg.document.file_id);
+        const stream = bot.getFileStream(msg.document.file_id);
 
-    await getReadStreamPromise(stream, fileWriter);
+        await getReadStreamPromise(stream, fileWriter);
 
-    await bot.sendMessage(chatId, "Файл подготовлен, Отправляем данные в ChatGPT");
+        await bot.sendMessage(chatId, "Файл подготовлен, Отправляем данные в ChatGPT");
 
-    Docx.extract("file.docx", "extracted");
+        Docx.extract("file.docx", "extracted");
 
-    const originalJSON = Docx.translateXMLToJSON();
+        const originalJSON = Docx.translateXMLToJSON();
 
-    const preparedJSON = Docx.prepareJSONForRephpasing(originalJSON);
+        const preparedJSON = Docx.prepareJSONForRephpasing(originalJSON);
 
-    const resultJSON = await getResult(chatId, preparedJSON);
+        const resultJSON = await getResult(chatId, preparedJSON);
 
-    isBusy = false;
+        isBusy = false;
 
-    Docx.translateJSONToXML(originalJSON, resultJSON);
+        Docx.translateJSONToXML(originalJSON, resultJSON);
 
-    Docx.create("extracted", "result.docx");
+        Docx.create("extracted", "result.docx");
 
-    await bot.sendMessage(chatId, "Результат готов");
+        await bot.sendMessage(chatId, "Результат готов");
 
-    const buffer = fs.readFileSync("result.docx");
+        const buffer = fs.readFileSync("result.docx");
 
-    await bot.sendDocument(chatId, buffer, {}, { filename: "output.docx", contentType: "text" });
-  } catch (error) {
-    isBusy = false;
-    console.log(error);
+        await bot.sendDocument(chatId, buffer, {}, { filename: "output.docx", contentType: "application/octet-stream" });
+    } catch (error) {
+        isBusy = false;
+        console.log(error);
 
-    if (!error.response) return await bot.sendMessage(chatId, "Произошла внутренняя ошибка, повторите позднее" + error);
+        if (!error.response) return await bot.sendMessage(chatId, "Произошла внутренняя ошибка, повторите позднее" + error);
 
-    if (!error.response.data) return await bot.sendMessage(chatId, "Произошла внутренняя ошибка, повторите позднее" + error);
+        if (!error.response.data) return await bot.sendMessage(chatId, "Произошла внутренняя ошибка, повторите позднее" + error);
 
-    console.log(error.response.data.error);
+        console.log(error.response.data.error);
 
-    if (error.response.data.error.type === "insufficient_quota") {
-      return await bot.sendMessage(
-        chatId,
-        "Вы использовали всю доступную квоту, ChatGPT недоступен с данного аккаунта, смените API_KEY, введя: API_KEY=ваш_ключ_api"
-      );
+        if (error.response.data.error.type === "insufficient_quota") {
+            return await bot.sendMessage(
+                chatId,
+                "Вы использовали всю доступную квоту, ChatGPT недоступен с данного аккаунта, смените API_KEY, введя: API_KEY=ваш_ключ_api"
+            );
+        }
+        if (error.response.status === 400) {
+            return await bot.sendMessage(
+                chatId,
+                "Некорректный запрос, в последний раз такая ошибка была, если в документе был очеь длинный параграф "
+            );
+        }
+        if (error.response.status === 401) {
+            return await bot.sendMessage(
+                chatId,
+                "Такого API_KEY не существует, создайте новый API_KEY в личном кабинете а затем смените API_KEY, введя: API_KEY=ваш_ключ_api"
+            );
+        }
+        if (error.response.status === 429) {
+            return await bot.sendMessage(chatId, "В данный момент обрабатывается другой документ, повторите позднее");
+        }
+        if (error.response.status === 500) {
+            return await bot.sendMessage(chatId, "Внутренняя ошибка ChatGPT, мы тут не причем, повтворите позднее");
+        }
+
+        return await bot.sendMessage(chatId, "Произошла внутренняя ошибка, повторите позднее" + error);
     }
-    if (error.response.status === 400) {
-      return await bot.sendMessage(
-        chatId,
-        "Некорректный запрос, в последний раз такая ошибка была, если в документе был очеь длинный параграф "
-      );
-    }
-    if (error.response.status === 401) {
-      return await bot.sendMessage(
-        chatId,
-        "Такого API_KEY не существует, создайте новый API_KEY в личном кабинете а затем смените API_KEY, введя: API_KEY=ваш_ключ_api"
-      );
-    }
-    if (error.response.status === 429) {
-      return await bot.sendMessage(chatId, "В данный момент обрабатывается другой документ, повторите позднее");
-    }
-    if (error.response.status === 500) {
-      return await bot.sendMessage(chatId, "Внутренняя ошибка ChatGPT, мы тут не причем, повтворите позднее");
-    }
-
-    return await bot.sendMessage(chatId, "Произошла внутренняя ошибка, повторите позднее" + error);
-  }
 });
 
 console.log("Bot is launched");
--- word/docx.js
+++ word/docx.js
@@ -3,220 +3,231 @@ const fs = require("fs");
 const { parseString, Builder } = require("xml2js");
 
 class Docx {
-  static extract(from, to) {
-    const zip = new AdmZip(from);
-    zip.extractAllTo(to, true);
-  }
-
-  static create(from, to) {
-    const zip = new AdmZip();
-
-    zip.addLocalFolder(from);
-
-    zip.writeZip(to);
-
-    fs.rm("./extracted", { recursive: true }, (err) => {
-      if (err) console.error(err);
-    });
-  }
-
-  static translateXMLToJSON() {
-    let json;
-    const xml = fs.readFileSync("./extracted/word/document.xml");
-    parseString(xml, function (err, result) {
-      json = result;
-    });
-
-    return json;
-  }
-
-  static translateJSONToXML(original, prepared) {
-    const builder = new Builder();
-
-    const { p } = this.getDocumentElements(original);
-
-    const wp = p.map((p, i) => {
-      if (!prepared[i].handle) return p;
-
-      // console.log(p);
-
-      let pStyle = p.hasOwnProperty("w:pPr") ? p["w:pPr"][0] : null;
-      if (pStyle && !pStyle.hasOwnProperty("w:numPr")) pStyle = { ...pStyle, ["w:ind"]: [{ $: { "w:firstLine": "708" } }] };
-
-      let rStyle = p["w:r"][0].hasOwnProperty("w:rPr") ? p["w:r"][0]["w:rPr"][0] : pStyle;
-      if (prepared[i].isAddText) rStyle = { ...rStyle, ["w:highlight"]: [{ $: { "w:val": "yellow" } }] };
-      if (rStyle && rStyle.hasOwnProperty("w:b")) delete rStyle["w:b"];
-
-      let wr = [
-        {
-          "w:rPr": [rStyle],
-          "w:t": [prepared[i].text],
-        },
-      ];
-      if (prepared[i].ref) wr.push(prepared[i].ref);
-
-      const result = {
-        ...p,
-        ["w:r"]: wr,
-      };
-
-      if (pStyle) result["w:pPr"] = [pStyle];
-
-      return result;
-    });
-
-    const changedDocument = this.changeParagraphs(original, wp);
-
-    const xml = builder.buildObject(changedDocument);
-
-    fs.writeFileSync("./extracted/word/document.xml", xml);
-  }
-
-  static checkIsCenter(paragraph) {
-    return (
-      paragraph.hasOwnProperty("w:pPr") &&
-      paragraph["w:pPr"][0].hasOwnProperty("w:jc") &&
-      paragraph["w:pPr"][0]["w:jc"][0]["$"]["w:val"] === "center"
-    );
-  }
-
-  static checkIsPicture(rows) {
-    return rows.findIndex((r) => r.hasOwnProperty("w:drawing")) > -1;
-  }
-
-  static checkColor = (rows, color) => {
-    let isAddText = false;
-
-    for (let i = 0; i < rows.length; i++) {
-      if (
-        rows[i].hasOwnProperty("w:rPr") &&
-        rows[i]["w:rPr"][0].hasOwnProperty("w:highlight") &&
-        rows[i]["w:rPr"][0]["w:highlight"][0]["$"]["w:val"] === color
-      ) {
-        isAddText = true;
-        break;
-      }
+    static extract(from, to) {
+        const zip = new AdmZip(from);
+        zip.extractAllTo(to, true);
     }
 
-    return isAddText;
-  };
+    static create(from, to) {
+        const zip = new AdmZip();
 
-  static checkIsBoldText = (row) => {
-    return row.hasOwnProperty("w:rPr") && row["w:rPr"][0].hasOwnProperty("w:b");
-  };
+        zip.addLocalFolder(from);
 
-  static getRowText = (row) => {
-    if (!row.hasOwnProperty("w:t")) return "";
-    return typeof row["w:t"][0] === "string" ? row["w:t"][0] : row["w:t"][0].hasOwnProperty("_") ? row["w:t"][0]["_"] : "";
-  };
+        zip.writeZip(to);
 
-  static findKeywords = (rows) => {
-    const keywords = [];
-    rows.forEach((r) => {
-      if (this.checkIsBoldText(r)) keywords.push(this.getRowText(r));
-    });
-    return keywords;
-  };
+        fs.rm("./extracted", { recursive: true }, (err) => {
+            if (err) console.error(err);
+        });
+    }
 
-  static getRef = (rows) => {
-    let ref = null;
-    rows.forEach((r) => {
-      if (r.hasOwnProperty("w:footnoteReference")) ref = r;
-    });
-    return ref;
-  };
+    static translateXMLToJSON() {
+        let json;
+        const xml = fs.readFileSync("./extracted/word/document.xml");
+        parseString(xml, function (err, result) {
+            json = result;
+        });
 
-  static prepareJSONForRephpasing(json) {
-    const { p } = this.getDocumentElements(json);
+        return json;
+    }
 
-    const result = p.map((p) => {
-      const obj = {
-        type: "empty",
-        handle: true,
-        pStyle: null,
-        isAddText: false,
-        ref: null,
-        text: "",
-        keywords: [],
-        query: "",
-      };
+    static translateJSONToXML(original, prepared) {
+        const builder = new Builder();
 
-      const rows = p["w:r"];
+        const { p } = this.getDocumentElements(original);
 
-      if (!rows) return { ...obj, handle: false };
+        const wp = p.map((p, i) => {
+            if (!prepared[i].handle) return p;
 
-      obj.type = "text";
+            // console.log(p);
 
-      const isHeader = this.checkIsCenter(p);
-      if (isHeader) obj.type = "center";
+            let pStyle = p.hasOwnProperty("w:pPr") ? p["w:pPr"][0] : null;
+            if (pStyle && !pStyle.hasOwnProperty("w:numPr")) pStyle = { ...pStyle, ["w:ind"]: [{ $: { "w:firstLine": "708" } }] };
 
-      const isPicture = this.checkIsPicture(rows);
-      if (isPicture) obj.type = "picture";
+            let rStyle = p["w:r"][0].hasOwnProperty("w:rPr") ? p["w:r"][0]["w:rPr"][0] : pStyle;
+            if (prepared[i].isAddText) rStyle = { ...rStyle, ["w:highlight"]: [{ $: { "w:val": "yellow" } }] };
+            if (rStyle && rStyle.hasOwnProperty("w:b")) delete rStyle["w:b"];
 
-      const isReady = this.checkColor(rows, "green");
-      if (isReady) obj.type = "ready";
+            let wr = [
+                {
+                    "w:rPr": [rStyle],
+                    "w:t": [prepared[i].text],
+                },
+            ];
+            if (prepared[i].ref) wr.push(prepared[i].ref);
 
-      if (isHeader || isPicture || isReady) return { ...obj, handle: false };
+            const result = {
+                ...p,
+                ["w:r"]: wr,
+            };
 
-      obj.pStyle = p["w:pPr"];
+            if (pStyle) result["w:pPr"] = [pStyle];
 
-      obj.keywords.push(...this.findKeywords(rows));
+            return result;
+        });
 
-      obj.isAddText = this.checkColor(rows, "yellow");
+        const changedDocument = this.changeParagraphs(original, wp);
 
-      obj.ref = this.getRef(rows);
+        const xml = builder.buildObject(changedDocument);
 
-      rows.forEach((r) => {
-        if (r.hasOwnProperty("w:t")) {
-          obj.text += this.getRowText(r);
-        }
-      });
+        fs.writeFileSync("./extracted/word/document.xml", xml);
+    }
 
-      obj.query = "Перефразируй текст";
+    static checkIsCenter(paragraph) {
+        return (
+            paragraph.hasOwnProperty("w:pPr") &&
+            paragraph["w:pPr"][0].hasOwnProperty("w:jc") &&
+            paragraph["w:pPr"][0]["w:jc"][0]["$"]["w:val"] === "center"
+        );
+    }
 
-      if (obj.keywords.length)
-        obj.query += `, обязательно используй следующие слова без изменения их падежа и местоположения в тексте - "${obj.keywords.join(
-          ", "
-        )}", если не знаешь как, верни исходный вариант`;
+    static checkIsPicture(rows) {
+        return rows.findIndex((r) => r.hasOwnProperty("w:drawing")) > -1;
+    }
 
-      if (obj.text.length > 1700) {
-        const arr = obj.text.split(".");
+    static checkColor = (rows, color) => {
+        let isAddText = false;
 
-        const length = obj.text.length;
-
-        obj.text = "";
-
-        let i = 0;
-
-        while (obj.text.length < 1700) {
-          obj.text += arr[i];
-          i++;
+        for (let i = 0; i < rows.length; i++) {
+            if (
+                rows[i].hasOwnProperty("w:rPr") &&
+                rows[i]["w:rPr"][0].hasOwnProperty("w:highlight") &&
+                rows[i]["w:rPr"][0]["w:highlight"][0]["$"]["w:val"] === color
+            ) {
+                isAddText = true;
+                break;
+            }
         }
 
-        obj.query += `, и допиши как минимум ${length - 1700} символов в том же стиле что и весь текст`;
-      } else if (obj.isAddText) obj.query += ", и допиши 5 предложений в том же стиле что и весь текст";
+        return isAddText;
+    };
 
-      return obj;
-    });
+    static checkIsBoldText = (row) => {
+        return row.hasOwnProperty("w:rPr") && row["w:rPr"][0].hasOwnProperty("w:b");
+    };
 
-    return result;
-  }
+    static getRowText = (row) => {
+        if (!row.hasOwnProperty("w:t")) return "";
+        return typeof row["w:t"][0] === "string" ? row["w:t"][0] : row["w:t"][0].hasOwnProperty("_") ? row["w:t"][0]["_"] : "";
+    };
 
-  static changeParagraphs(json, wp) {
-    const { document, body } = this.getDocumentElements(json);
+    static findKeywords = (rows) => {
+        const keywords = [];
+        rows.forEach((r) => {
+            if (this.checkIsBoldText(r)) keywords.push(this.getRowText(r));
+        });
+        return keywords;
+    };
 
-    return { ...json, ["w:document"]: { ...document, ["w:body"]: [{ ...body, ["w:p"]: wp }] } };
-  }
+    static getRef = (rows) => {
+        let ref = null;
+        rows.forEach((r) => {
+            if (r.hasOwnProperty("w:footnoteReference")) ref = r;
+        });
+        return ref;
+    };
 
-  static getDocumentElements(json) {
-    const document = json["w:document"];
+    static prepareJSONForRephpasing(json) {
+        const { p } = this.getDocumentElements(json);
 
-    const body = document["w:body"][0];
+        const result = p.map((p) => {
+            const obj = {
+                type: "empty",
+                handle: true,
+                pStyle: null,
+                isAddText: false,
+                ref: null,
+                text: "",
+                keywords: [],
+                query_1: "",
+                query_2: "",
+                query: "",
+            };
 
-    const p = body["w:p"];
+            const rows = p["w:r"];
 
-    return { document, body, p };
-  }
+            if (!rows) return { ...obj, handle: false };
+
+            obj.type = "text";
+
+            const isHeader = this.checkIsCenter(p);
+            if (isHeader) obj.type = "center";
+
+            const isPicture = this.checkIsPicture(rows);
+            if (isPicture) obj.type = "picture";
+
+            const isReady = this.checkColor(rows, "green");
+            if (isReady) obj.type = "ready";
+
+            if (isHeader || isPicture || isReady) return { ...obj, handle: false };
+
+            obj.pStyle = p["w:pPr"];
+
+            obj.keywords.push(...this.findKeywords(rows));
+
+            obj.isAddText = this.checkColor(rows, "yellow");
+
+            obj.ref = this.getRef(rows);
+
+            rows.forEach((r) => {
+                if (r.hasOwnProperty("w:t")) {
+                    obj.text += this.getRowText(r);
+                }
+            });
+
+            // obj.query_1 =
+            //     "Перепиши текст на английский язык так, чтобы он был оригинальным и проходил проверку сервисов, которые определяют, что текст сгенерировала нейросеть";
+            // obj.query_2 = "Переведи этот текст на русский язык и исправь в нем грамматические ошибки. В ответе пришли только русский язык";
+
+            obj.query_1 =
+                "Перепиши текст на английский язык так, чтобы он был оригинальным и проходил проверку сервисов, которые определяют, что текст сгенерировала нейросеть";
+            obj.query_2 =
+                "Переведи этот текст на русский язык. Сделай его более читаемым и вновь сделай так, чтобы он был оригинальным и проходил проверку сервисов, которые определяют, что текст сгенерировала нейросеть. В ответе пришли только русский язык";
+
+            obj.query =
+                "Прошу тебя выступить в роли профессионального копирайтера и переосмыслить предоставленный текст, используя синонимы, чтобы создать уникальную версию, в которой используется естественный стиль. Пожалуйста, сохраните исходное форматирование и смысл текста, придерживаясь красивого литературного языка и избегая стилистических и грамматических ошибок. Обратите внимание, что общий объем текста не должен превышать исходный более чем на 10%. Предоставленный текст будет разделен на части, однако помните, что он образует единую целостность. Пожалуйста, обеспечь высокую сложность и разнообразие контента, чтобы избежать повторов и успешно пройти проверку на уникальность.";
+            // if (obj.keywords.length)
+            //   obj.query += `, обязательно используй следующие слова без изменения их падежа и местоположения в тексте - "${obj.keywords.join(
+            //     ", "
+            //   )}", если не знаешь как, верни исходный вариант`;
+
+            // if (obj.text.length > 1700) {
+            //   const arr = obj.text.split(".");
+
+            //   const length = obj.text.length;
+
+            //   obj.text = "";
+
+            //   let i = 0;
+
+            //   while (obj.text.length < 1700) {
+            //     obj.text += arr[i];
+            //     i++;
+            //   }
+
+            //   obj.query += `, и допиши как минимум ${length - 1700} символов в том же стиле что и весь текст`;
+            // } else if (obj.isAddText) obj.query += ", и допиши 5 предложений в том же стиле что и весь текст";
+
+            return obj;
+        });
+
+        return result;
+    }
+
+    static changeParagraphs(json, wp) {
+        const { document, body } = this.getDocumentElements(json);
+
+        return { ...json, ["w:document"]: { ...document, ["w:body"]: [{ ...body, ["w:p"]: wp }] } };
+    }
+
+    static getDocumentElements(json) {
+        const document = json["w:document"];
+
+        const body = document["w:body"][0];
+
+        const p = body["w:p"];
+
+        return { document, body, p };
+    }
 }
 
 module.exports = Docx;
